f342189027b3a8c0329608f6c96b506a5d563d41
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-11-11 11:45:36 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app=express()
 app.use(cors({
-  origin: ["https://ecommerce-next-eosin-tau.vercel.app", "https://ecommerce-react-three-psi.vercel.app/"],
+  origin: ["https://ecommerce-next-eosin-tau.vercel.app", "https://ecommerce-react-three-psi.vercel.app"],
   credentials: true,
 }));
  app.use(cookieParser());
